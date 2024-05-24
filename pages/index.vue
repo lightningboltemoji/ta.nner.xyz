4340f272e62de76477852f779b4dcd7f89a3ce8d
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-05-23 22:04:56 -0700

    Add Lax.js animations on scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,77 @@
 <script setup lang="ts">
+import lax from "lax.js";
+
 const show = ref(false);
 
 onMounted(() => {
     show.value = true;
+
+    // Workaround https://github.com/alexfoxy/lax.js/issues/156
+    window.lax = { presets: lax.presets };
+
+    lax.init();
+    lax.addDriver("scrollY", () => window.scrollY);
+    lax.addElements(".explodeAfterFirst", {
+        scrollY: {
+            scaleX: [
+                ["screenHeight/2", "screenHeight"],
+                [1, "(screenWidth + screenHeight)/100"],
+                { easing: "easeInOut" },
+            ],
+            scaleY: [
+                ["screenHeight/2", "screenHeight"],
+                [1, "(screenWidth + screenHeight)/100"],
+                { easing: "easeInOut" },
+            ],
+        },
+    });
+    lax.addElements(".fadeInOut", {
+        scrollY: {
+            opacity: [["elInY", "elInY+elHeight", "elOutY-elHeight", "elOutY"], [0, 1, 1, 0], { easing: "easeInOut" }],
+        },
+    });
 });
 </script>
 
 <template>
-    <div class="flex flex-col justify-center items-center min-w-screen min-h-screen">
-        <div class="relative">
-            <video
-                class="size-[200px] bg-black rounded-full"
-                autoPlay
-                loop
-                muted
-                playsInline
-                src="/public/memoji.mp4?url"
-            />
-        </div>
-        <h1 class="font-mono text-3xl mt-7" ref="name">
-            <type-effect start text="Tanner Cecchetti" />
-        </h1>
-        <div class="flex flex-col items-center mt-5">
-            <Transition name="slide-fade" appear v-if="show">
-                <h2><span class="opacity-20">a</span> Software Engineer 👨‍💻</h2>
-            </Transition>
-            <h2 v-else class="invisible">👨‍💻</h2>
-            <Transition name="slide-fade" appear v-if="show">
-                <h2><span class="opacity-20">in</span> Seattle, WA 🌳</h2>
-            </Transition>
-            <h2 v-else class="invisible">🌳</h2>
-        </div>
+    <div class="flex justify-center items-center min-w-screen">
+        <div class="fixed bg-black rounded-full w-[200px] h-[200px] top-[100vh] explodeAfterFirst" />
     </div>
     <div class="flex flex-col justify-center items-center min-w-screen min-h-screen">
-        <h1 class="font-mono text-3xl mt-7">
-            <type-effect text="Work experience" />
-        </h1>
-        <h2>Amazon Web Services, 2022 -</h2>
-        <h2>HealthSparq, 2015 - 2022</h2>
+        <div class="flex flex-col justify-center items-center fadeInOut">
+            <div class="relative">
+                <video
+                    class="size-[200px] bg-black rounded-full"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    src="/public/memoji.mp4?url"
+                />
+            </div>
+            <h1 class="font-mono text-3xl mt-7" ref="name">
+                <type-effect start text="Tanner Cecchetti" />
+            </h1>
+            <div class="flex flex-col items-center mt-5">
+                <Transition name="slide-fade" appear v-if="show">
+                    <h2><span class="opacity-20">a</span> Software Engineer 👨‍💻</h2>
+                </Transition>
+                <h2 v-else class="invisible">👨‍💻</h2>
+                <Transition name="slide-fade" appear v-if="show">
+                    <h2><span class="opacity-20">in</span> Seattle, WA 🌳</h2>
+                </Transition>
+                <h2 v-else class="invisible">🌳</h2>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col justify-center items-center min-w-screen min-h-screen text-white">
+        <div class="flex flex-col justify-center items-center fadeInOut">
+            <h1 class="font-mono text-3xl mt-7">
+                <type-effect text="Work experience" />
+            </h1>
+            <h2>Amazon Web Services, 2022 -</h2>
+            <h2>HealthSparq, 2015 - 2022</h2>
+        </div>
     </div>
 </template>
 
