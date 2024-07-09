2660995e9d552ce4587c73142f158623a3c71370
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-07-08 22:18:31 -0700

    Rename variable

--- app/expanding-ball.tsx
+++ app/expanding-ball.tsx
@@ -9,14 +9,14 @@ export default function ExpandingBall({ anchorTo }: { anchorTo: RefObject<HTMLEl
   if (typeof window !== "undefined") {
     dim = { w: window.innerWidth, h: window.innerHeight };
   }
-  const maxDia = Math.ceil(Math.sqrt((dim.w / 2) ** 2 + dim.h ** 2)) * 2;
+  const diaMax = Math.ceil(Math.sqrt((dim.w / 2) ** 2 + dim.h ** 2)) * 2;
 
   // Center the ball at middle of the the bottom on the screen
   const { scrollYProgress } = useScroll({ target: anchorTo, offset: ["0.25 end", "0.75 end"] });
   const spring = useSpring(scrollYProgress, { damping: 12, bounce: 0 });
-  const dia = useTransform(spring, [0, 1], [0, maxDia]);
+  const dia = useTransform(spring, [0, 1], [0, diaMax]);
   const left = useTransform(() => dim.w / 2 - dia.get() / 2);
-  const top = useTransform(spring, [0, 1], [dim.h, dim.h - maxDia / 2]);
+  const top = useTransform(spring, [0, 1], [dim.h, dim.h - diaMax / 2]);
 
   return <motion.div className="fixed bg-black rounded-full" style={{ top, left, width: dia, height: dia }} />;
 }
