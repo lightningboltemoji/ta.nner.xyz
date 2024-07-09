54d78fc11a9cece3be9dd812cc07eb3bf7ec37ee
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-07-08 20:18:49 -0700

    Get started with framer-motion, add Prettier import plugin

---
+++ app/work-edu-card.tsx
@@ -0,0 +1,17 @@
+export default function WorkEduCard() {
+  return (
+    <div id="edu" className="flex flex-col justify-center items-center min-w-screen min-h-screen text-white">
+      <div className="flex flex-col justify-center items-center fade-in-out">
+        <h1 className="font-mono text-3xl mt-7">Work experience</h1>
+        <h2>Amazon Web Services, 2022 -</h2>
+        <h2>HealthSparq, 2015 - 2022</h2>
+      </div>
+
+      <div className="flex flex-col justify-center items-center fade-in-out">
+        <h1 className="font-mono text-3xl mt-7">Education</h1>
+        <h2>Oregon State University, 2017</h2>
+        <h2>Computer Science, B.S.</h2>
+      </div>
+    </div>
+  );
+}
