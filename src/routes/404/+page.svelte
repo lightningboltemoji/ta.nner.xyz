8ff8ba21c5694f504cc4259c021f7bc0ce559941
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-11-20 21:50:59 -0800

    Tweak styling of 404

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-col w-screen h-screen font-mono justify-center items-center">
-  <h1 class="text-3xl">Not found</h1>
-  <h2 class="text-4xl">😔</h2>
+  <h1 class="text-3xl -mt-32">Not found</h1>
+  <h2 class="text-4xl pt-3">😦</h2>
 </div>
