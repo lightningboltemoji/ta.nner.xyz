6ef5db630c18abb2097dfda3559e37bf16c2cf1c
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-11-23 22:32:02 -0800

    Package bumps, add WebSite element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,14 @@
     name="description"
     content="Full-stack software engineer based in Seattle, WA. Passionate about elegant solutions that do more with less. Find all my links here."
   />
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "WebSite",
+      "name": "Tanner Cecchetti",
+      "url": "https://ta.nner.xyz/"
+    }
+  </script>
 </svelte:head>
 
 <CardHeader />
